89a291c6db55b4c4fd4b8273027caf6adc67a1e6
DerexScript <Derex@outlook.com.br>
2018-07-18 19:34:20 -0300

    Second Commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const webdriver = require('selenium-webdriver'), By = webdriver.By, until = webdriver.until, keys = webdriver.keys;
+const webdriver = require('selenium-webdriver'),
+	By = webdriver.By,
+	until = webdriver.until,
+	keys = webdriver.keys;
 let chrome = require('selenium-webdriver/chrome');
 let options = new chrome.Options();
 
 options.addArguments("user-data-dir=c:/Users/Derex/AppData/Local/Google/Chrome/User Data/DC/");
-let driver = new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
+let driver = new webdriver.Builder()
+	.forBrowser('chrome')
+	.setChromeOptions(options)
+	.build();
 
 driver.get('http://www.facebook.com/groups/');
 
@@ -12,32 +18,16 @@ let getLinksGroups = (() => {
 	return new Promise(resolve => {
 		driver.findElement({ css: '.uiList.mam._509-._4ki._4ks' }).then((ncol) => {
 			ncol.findElements({ xpath: './li' }).then((lis) => {
-
-				const results = lis.map((obj) => {
-					obj.findElements({ css: 'li>ul>li' }).then((lisgroups) => {
-						lisgroups[k].getAttribute("id").then((idattr) => {
-
+				lis.map((elem, index, lis) => {
+					lis[index].findElements({ css: 'li>ul>li' }).then((lisgroups) => {
+						var arrLinkGroups = lisgroups.map((elem1, index1, lisgroups) => {
+							elem1.getAttribute("id").then((idattr) => {
+								return /[^\_]+\_([^\n]+)/.exec(idattr)[1];
+							});
 						});
 					});
 				});
-
-				console.log(results);
-				/*
-				for (let i = 0; i < lis.length; i++) {
-					lis[i].findElements({ css: 'li>ul>li' }).then((lisgroups) => {
-						for (let k = 0; k < lisgroups.length; k++) {
-							lisgroups[k].getAttribute("id").then((idattr) => {
-								console.log(/[^\_]+\_([^\n]+)/.exec(idattr)[1]);
-							});
-							if (i == lis.length - 1 && k == lisgroups.length - 1) {
-								//console.log(arrLinkGroups);
-							}
-						}
-					});
-				}*/
-
-
-			})
+			});
 		});
 	});
 })
@@ -45,7 +35,7 @@ let getLinksGroups = (() => {
 driver.wait(until.elementLocated({ css: '._38my' })).then(() => {
 	getLinksGroups().then((linkGroups) => {
 		console.log(linkGroups);
-	})
+	});
 });
 
 setTimeout(() => {
