e82faa1ef634741faf52e83c66e8b9b73b5ae064
DerexScript <Derex@outlook.com.br>
2018-07-19 00:15:06 -0300

    ....

--- index.js
+++ index.js
@@ -17,16 +17,15 @@ driver.get('http://www.facebook.com/groups/');
 let getLinksGroups = (() => {
 	driver.findElement({ css: '.uiList.mam._509-._4ki._4ks' }).then((ncol) => {
 		ncol.findElements({ xpath: './li' }).then((lis) => {
-			let promiseone = lis.map((elem, index, lis) => {
+			lis.map((elem, index, lis) => {
 				lis[index].findElements({ css: 'li>ul>li' }).then((lisgroups) => {
-					let arrLinkGroups = lisgroups.map(async (elem1, index1, lisgroups) => {
+					lisgroups.map((elem1, index1, lisgroups) => {
 						elem1.getAttribute("id").then((idattr) => {
 							return /[^\_]+\_([^\n]+)/.exec(idattr)[1];
 						});
-					});
+					})
 				});
 			});
-			Promise.all(promiseone).then( );
 		});
 	});
 });
